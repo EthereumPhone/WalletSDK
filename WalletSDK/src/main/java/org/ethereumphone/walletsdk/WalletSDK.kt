73ddb7a3cc75b8b9afaf87b01791308df03b0daf
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-10-14 13:03:11 +0200

    added signMessage/SendTransaction

--- WalletSDK/src/main/java/org/ethereumphone/walletsdk/WalletSDK.kt
+++ WalletSDK/src/main/java/org/ethereumphone/walletsdk/WalletSDK.kt
@@ -2,23 +2,69 @@ package org.ethereumphone.walletsdk
 
 import android.content.Context
 import dev.pinkroom.walletconnectkit.WalletConnectKitConfig
+import dev.pinkroom.walletconnectkit.WalletConnectKit
+import kotlin.concurrent.thread
+
 
 const val SYS_SERVICE = "android.os.WalletProxy"
 
-
 class WalletSDK(
     context: Context,
+    walletConnectKitConfig: WalletConnectKitConfig? = null
 
 ) {
-    var cls: Class<*>? = Class.forName(SYS_SERVICE)
-    var proxy = context.getSystemService("wallet")
+    private val cls: Class<*>? = Class.forName(SYS_SERVICE)
+    private val proxy = context.getSystemService("wallet")
+    private val config = walletConnectKitConfig ?: WalletConnectKitConfig(
+        context = context,
+        bridgeUrl = "wss://bridge.aktionariat.com:8887",
+        appUrl = "https://ethereumphone.org",
+        appName = "WalletSDK",
+        appDescription = ""
+    )
+    private var walletConnectKit: WalletConnectKit? = null
 
+    init {
+        walletConnectKit = WalletConnectKit.Builder(config).build()
+    }
 
     /**
      * Sends transaction to
      */
-    fun performTransaction() {
-        val connect = cls?.getMethod("performTransaction")
+
+    //TODO: request id check for request completion
+    fun sendTransaction(): String {
+        if(proxy != null) {
+            val sendTransaction = cls?.getMethod("sendTransaction")
+            val hasBeenFulfilled = cls?.getMethod("hasBeenFulfilled")
+            var result =  "notfulfilled"
+
+            thread {
+                // result = hasBeenFulfilled!!.invoke(proxy) as String
+                if(result != "notfulfilled") {
+                    Thread.interrupted()
+                }
+            }
+            return result
+        }
+        return ""
+    }
+
+    fun signMessage(): String {
+        if(proxy != null) {
+            val signMessage = cls?.getMethod("signMessage")
+            val hasBeenFulfilled = cls?.getMethod("hasBeenFulfilled")
+            var result =  "notfulfilled"
+
+            thread {
+                // result = hasBeenFulfilled!!.invoke(proxy) as String
+                if(result != "notfulfilled") {
+                    Thread.interrupted()
+                }
+            }
+            return result
+        }
+        return ""
     }
 
     /**
@@ -30,7 +76,6 @@ class WalletSDK(
             val createSession = cls?.getMethod("createSession")
             return createSession!!.invoke(proxy) as String
         }
-
         return ""
     }
 }
