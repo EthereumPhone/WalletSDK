66a6eec007a269d3042e90ba472dc3619fddc548
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2023-06-02 20:56:58 -0700

    Changed WalletSDK from completableFutures to coroutines. Transfers from other chains are now supported.

--- WalletSDK/src/main/java/org/ethereumphone/walletsdk/WalletSDK.kt
+++ WalletSDK/src/main/java/org/ethereumphone/walletsdk/WalletSDK.kt
@@ -2,194 +2,144 @@ package org.ethereumphone.walletsdk
 
 import android.annotation.SuppressLint
 import android.content.Context
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.ethereumphone.walletsdk.model.NoSysWalletException
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.core.DefaultBlockParameterName
 import org.web3j.protocol.http.HttpService
-import java.util.concurrent.CompletableFuture
 
+@SuppressLint("WrongConstant") // need this to make Android Studio behave
 class WalletSDK(
-    context: Context,
-    web3RPC: String = "https://rpc.ankr.com/eth"
-)  {
+    private val context: Context,
+    private var web3jInstance: Web3j = Web3j.build(HttpService("https://rpc.ankr.com/eth")), // starts on mainnet
+) {
+    // System service methods
+    private val cls: Class<*> = Class.forName(SYS_SERVICE_CLASS)
+    private val changeChainId = cls.declaredMethods[1]
+    private val createSession = cls.declaredMethods[2]
+    private val getAddress = cls.declaredMethods[3]
+    private val getChainId = cls.declaredMethods[4]
+    private val getUserDecision = cls.declaredMethods[5]
+    private val hasBeenFulfilled = cls.declaredMethods[6]
+    private val sendTransaction =  cls.declaredMethods[7]
+    private val signMessageSys = cls.declaredMethods[8]
+
+    private var address: String? = null
+    private var proxy : Any? = null
+    private var sysSession: String? = null
+
+    init {
+        proxy = initializeProxyService()
+        sysSession = createSession.invoke(proxy) as String
+
+        CoroutineScope(Dispatchers.IO).launch {
+            address = initialAddressRequest()
+        }
+    }
+
+    /**
+     * In case the system-service is not found, it throws an execption in the init body of the WalletSDK, so that I don't need to check the proxy on every function
+     */
+    private fun initializeProxyService() = context.getSystemService(SYS_SERVICE) ?: throw NoSysWalletException("System wallet not found on this device")
+
+    /**
+     * Wallet only supports one address as of right now, thus we only need to get the wallet address once
+     */
+    private suspend fun initialAddressRequest(): String = coroutineScope {
+        val deferredResult = async {
+            val reqId = getAddress.invoke(proxy, sysSession) as String
+            while((hasBeenFulfilled.invoke(proxy, reqId) as String) == NOTFULFILLED) {
+                delay(10)
+            }
+            hasBeenFulfilled.invoke(proxy, reqId) as String
+        }
+
+        deferredResult.await()
+    }
+
+    suspend fun sendTransaction(
+        to: String,
+        value: String,
+        data: String,
+        gasPrice: String? = null,
+        gasAmount: String = "21000"
+    ): String = coroutineScope {
+        val deferredResult = async {
+            val result: String
+            val gas = if (gasPrice.isNullOrBlank()) {
+                web3jInstance.ethGasPrice().send().gasPrice.toString()
+            } else gasPrice
+            val ethGetTransactionCount = web3jInstance.ethGetTransactionCount(address, DefaultBlockParameterName.LATEST).send()
+            val reqId = sendTransaction.invoke(proxy, sysSession, to, value, data, ethGetTransactionCount.transactionCount.toString(), gas, gasAmount, 0)
+
+            while((hasBeenFulfilled.invoke(proxy, reqId) as String) == NOTFULFILLED) {
+                delay(10)
+            }
+            result = hasBeenFulfilled.invoke(proxy, reqId) as String
+
+            if (result == DECLINE) DECLINE else web3jInstance.ethSendRawTransaction(result).send().transactionHash
+        }
+
+        deferredResult.await()
+    }
+
+    suspend fun signMessage(message: String, type: String = "personal_sign"): String = coroutineScope {
+
+        val deferredResult = async {
+            val reqId = signMessageSys.invoke(proxy, sysSession, message, type) as String
+            while((hasBeenFulfilled.invoke(proxy, reqId) as String) == NOTFULFILLED) {
+                delay(10)
+            }
+            hasBeenFulfilled.invoke(proxy, reqId) as String
+        }
+        deferredResult.await()
+    }
+
+
+    suspend fun getChainId(): Int = coroutineScope {
+
+        val deferredResult = async {
+            val reqId = getChainId.invoke(proxy, sysSession) as String
+            while((hasBeenFulfilled.invoke(proxy, reqId) as String) == NOTFULFILLED) {
+                delay(10)
+            }
+            Integer.parseInt(hasBeenFulfilled.invoke(proxy, reqId) as String)
+        }
+        deferredResult.await()
+    }
+
+    /**
+     *
+     */
+    suspend fun changeChain(
+        chainId: Int,
+        rpcEndpoint: String
+    ): String = coroutineScope {
+
+        val deferredResult = async {
+            web3jInstance = Web3j.build(HttpService(rpcEndpoint))
+            val reqId = changeChainId.invoke(proxy, sysSession, chainId) as String
+            while((hasBeenFulfilled.invoke(proxy, reqId) as String) == NOTFULFILLED) {
+                delay(10)
+            }
+            hasBeenFulfilled.invoke(proxy, reqId) as String
+        }
+        deferredResult.await()
+    }
+
+    fun isEthOS(): Boolean {
+        return proxy != null
+    }
 
     companion object {
         const val SYS_SERVICE_CLASS = "android.os.WalletProxy"
         const val SYS_SERVICE = "wallet"
         const val DECLINE = "decline"
         const val NOTFULFILLED = "notfulfilled"
-    }
-
-
-    private val cls: Class<*> = Class.forName(SYS_SERVICE_CLASS)
-    private val createSession = cls.declaredMethods[2]
-    private val getUserDecision = cls.declaredMethods[5]
-    private val hasBeenFulfilled = cls.declaredMethods[6]
-    private val sendTransaction =  cls.declaredMethods[7]
-    private val signMessageSys = cls.declaredMethods[8]
-    private val getAddress = cls.declaredMethods[3]
-    private val getChainId = cls.declaredMethods[4]
-    private val changeChainId = cls.declaredMethods[1]
-    private var address: String? = null
-    @SuppressLint("WrongConstant")
-    private val proxy = context.getSystemService(SYS_SERVICE)
-    private var web3j: Web3j? = null
-    private var sysSession: String? = null
-
-    init {
-        if (proxy == null) {
-            throw NoSysWalletException("No system wallet found")
-        } else {
-            sysSession = createSession.invoke(proxy) as String
-            val reqID = getAddress.invoke(proxy, sysSession) as String
-            while ((hasBeenFulfilled.invoke(proxy, reqID) as String) == NOTFULFILLED) {
-                Thread.sleep(10)
-            }
-            address = hasBeenFulfilled.invoke(proxy, reqID) as String
-        }
-        web3j = Web3j.build(HttpService(web3RPC))
-    }
-
-    /**
-     * Sends transaction to
-     */
-
-    fun sendTransaction(to: String, value: String, data: String, gasPrice: String? = null, gasAmount: String = "21000", chainId: Int = 1): CompletableFuture<String> {
-        val completableFuture = CompletableFuture<String>()
-        var gasPriceVAL = gasPrice
-        if(proxy != null) {
-            // Use system-wallet
-
-            CompletableFuture.runAsync {
-                val ethGetTransactionCount = web3j!!.ethGetTransactionCount(
-                    address, DefaultBlockParameterName.LATEST
-                ).sendAsync().get()
-
-                if (gasPrice == null) {
-                    gasPriceVAL = web3j?.ethGasPrice()?.sendAsync()?.get()?.gasPrice.toString()
-                }
-
-                val reqID = sendTransaction.invoke(proxy, sysSession, to, value, data, ethGetTransactionCount.transactionCount.toString(), gasPriceVAL, gasAmount, chainId)
-
-                var result = NOTFULFILLED
-
-                while (true) {
-                    val tempResult =  hasBeenFulfilled!!.invoke(proxy, reqID)
-                    if (tempResult != null) {
-                        result = tempResult as String
-                        if(result != NOTFULFILLED) {
-                            break
-                        }
-                    }
-                    Thread.sleep(100)
-                }
-                if (result == DECLINE) {
-                    completableFuture.complete(DECLINE)
-                } else {
-                    val txResult = web3j!!.ethSendRawTransaction(result).sendAsync().get()
-                    val txHash = txResult.transactionHash
-                    completableFuture.complete(txHash)
-                }
-            }
-            return completableFuture
-        } else {
-            throw NoSysWalletException("No system wallet found")
-        }
-    }
-
-    fun signMessage(message: String, type: String = "personal_sign"): CompletableFuture<String> {
-        val completableFuture = CompletableFuture<String>()
-        if(proxy != null) {
-            CompletableFuture.runAsync {
-                val reqID = signMessageSys.invoke(proxy, sysSession, message, type) as String
-
-                var result =  NOTFULFILLED
-
-                while (true) {
-                    val tempResult =  hasBeenFulfilled!!.invoke(proxy, reqID)
-                    if (tempResult != null) {
-                        result = tempResult as String
-                        if(result != NOTFULFILLED) {
-                            break
-                        }
-                    }
-                    Thread.sleep(100)
-                }
-                completableFuture.complete(result)
-            }
-
-            return completableFuture
-        } else {
-            throw NoSysWalletException("No system wallet found")
-        }
-    }
-
-    /**
-     * Creates connection to the Wallet system service.
-     * If wallet is not found, user is redirect to WalletConnect login
-     */
-    fun createSession(onConnected: ((address: String) -> Unit)? = null): String {
-        if(proxy != null) {
-            onConnected?.let { it(sysSession.orEmpty()) }
-            return sysSession.orEmpty()
-        } else {
-            throw NoSysWalletException("No system wallet found")
-        }
-    }
-
-
-    fun getAddress(): String {
-        if (proxy != null) {
-            return address.orEmpty()
-        } else {
-            throw NoSysWalletException("No system wallet found")
-        }
-    }
-
-    fun getChainId(): Int {
-        if (proxy != null) {
-            val completableFuture = CompletableFuture<Int>()
-            CompletableFuture.runAsync {
-                val reqId = getChainId.invoke(proxy, sysSession) as String
-                while ((hasBeenFulfilled.invoke(proxy, reqId) as String) == NOTFULFILLED) {
-                    Thread.sleep(10)
-                }
-                completableFuture.complete(Integer.parseInt(hasBeenFulfilled.invoke(proxy, reqId) as String))
-            }
-            return completableFuture.get()
-        } else {
-            throw NoSysWalletException("No system wallet found")
-        }
-    }
-
-    fun changeChainid(chainId: Int): CompletableFuture<String> {
-        val completableFuture = CompletableFuture<String>()
-        if(proxy != null) {
-            CompletableFuture.runAsync {
-                val reqID = changeChainId.invoke(proxy, sysSession, chainId) as String
-
-                var result =  NOTFULFILLED
-
-                while (true) {
-                    val tempResult =  hasBeenFulfilled!!.invoke(proxy, reqID)
-                    if (tempResult != null) {
-                        result = tempResult as String
-                        if(result != NOTFULFILLED) {
-                            break
-                        }
-                    }
-                    Thread.sleep(100)
-                }
-                completableFuture.complete(result)
-            }
-
-            return completableFuture
-        } else {
-            throw NoSysWalletException("No system wallet found")
-        }
-    }
-
-    fun isEthOS(): Boolean {
-        return proxy != null
     }
 }
--- app/src/main/java/org/ethereumphone/walletsdk/MainActivity.kt
+++ app/src/main/java/org/ethereumphone/walletsdk/MainActivity.kt
@@ -1,48 +1,40 @@
 package org.ethereumphone.walletsdk
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import org.web3j.protocol.Web3j
-import org.web3j.protocol.core.DefaultBlockParameterName
-import org.web3j.protocol.http.HttpService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         val wallet = WalletSDK(this)
-        println("Chain ID: ${wallet.getChainId()}")
-        wallet.changeChainid(5).whenComplete { s, throwable ->
-             if (throwable != null) {
-                println("Error: $throwable")
-            } else {
-                println("Chain ID: ${wallet.getChainId()}")
-            }
-        }
-        wallet.signMessage(
-            message = "Launch control, this is Houston. We have go for launch."
-        ).whenComplete { s, throwable ->
-            if (s == WalletSDK.DECLINE) {
-                println("Signing declined")
-            } else {
-                println("Signed message: $s")
-            }
-        }
-        wallet.sendTransaction(
-            to = "0x3a4e6ed8b0f02bfbfaa3c6506af2db939ea5798c", // mhaas.eth
-            value = "1000000000000000000", // One eth in wei
-            data = ""
-        ).whenComplete {t, bhrowable ->
-            // Returns tx-id
-            if (t == WalletSDK.DECLINE) {
-                println("Send transaction has been declined")
-            } else {
-                println(t)
-            }
-        }
 
+        CoroutineScope(Dispatchers.IO).launch {
+            val res1 = wallet.changeChain(
+                5,
+                "https://rpc.ankr.com/eth_goerli"
+            )
+            Log.d("changeChain", res1)
+
+            val res2 = wallet.signMessage("Launch control, this is Houston. We have go for launch.")
+            Log.d("sign", res2)
+
+            val res3 = wallet.sendTransaction(
+                to = "0x3a4e6ed8b0f02bfbfaa3c6506af2db939ea5798c",
+                value = "1000000000000000000", // One eth in wei
+                data = "",
+            )
+            Log.d("sendTransaction", res3)
+        }
     }
 }
+
 
 
